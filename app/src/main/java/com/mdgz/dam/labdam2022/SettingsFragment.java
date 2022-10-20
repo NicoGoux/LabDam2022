85f357cf0fb33d9ceb21ddf1f9a1404e27ced005
Nicolas Goux <nicolasgoux2000@gmail.com>
2022-10-20 08:31:32 -0300

    settingsFragment

--- app/src/main/java/com/mdgz/dam/labdam2022/SettingsFragment.java
+++ app/src/main/java/com/mdgz/dam/labdam2022/SettingsFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.preference.CheckBoxPreference;
